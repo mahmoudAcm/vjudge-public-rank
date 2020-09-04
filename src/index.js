6115482b4fa066b25fdb0d08602c3b1528f93999
Mahmoud <mahmoud.acm.123@gmail.com>
2020-09-04 12:42:08 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -7,43 +7,44 @@ const port = process.env.PORT || 4000;
 
 app.get('/vjudge?', (req, res) => {
    console.log('here');
-   res.send('hi');
    try {
       (async () => {
-         const browser = await puppeteer.launch({ args: puppeteer.defaultArgs(), executablePath: puppeteer.executablePath(), });
+         const browser = await puppeteer.launch({headless: false});
          const page = await browser.newPage();
          await page.setDefaultTimeout(2000000);
          await page.setDefaultNavigationTimeout(0);
          await page.goto(`${req.query.url}#rank`);
 
-         const html = await page.$eval('#contest-rank-table', el => el.outerHTML);
-
-         const $ = cheerio.load(html);
-
-         let list = [];
-
-         let cur = 0;
-         $(".solved").each((index, el) => {
-            list.push({
-               count: $(el).text(),
-               handle: ''
-            });
-         });
-
-
-         $(".team a").each((index, el) => {
-            const string = $(el).attr('href');
-            let handle = string.split("/");
-            list[cur]["handle"] = handle[2];
-            cur++;
-         });
-
-         console.log(list);
+         // const html = await page.$eval('#contest-rank-table', el => el.outerHTML);
+         //
+         // const $ = cheerio.load(html);
+         //
+         // let list = [];
+         //
+         // let cur = 0;
+         // $(".solved").each((index, el) => {
+         //    list.push({
+         //       count: $(el).text(),
+         //       handle: ''
+         //    });
+         // });
+         //
+         //
+         // $(".team a").each((index, el) => {
+         //    const string = $(el).attr('href');
+         //    let handle = string.split("/");
+         //    list[cur]["handle"] = handle[2];
+         //    cur++;
+         // });
+         //
+         // console.log(list);
          await browser.close();
          console.log('done');
       })();
    } catch (e) {
-      res.send('error occurred');
+      console.log('err');
+   } finally {
+      res.send('hi');
    }
 });
 
